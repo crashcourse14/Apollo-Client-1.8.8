1ac29f63988cc11875d47b749b7d1768d385ef5e
C3real <haydyncm@gmail.com>
2026-06-15 15:50:00 +0000

    MSC Update #5 | revamped the theme of the click gui, and added commit/date into main menu string

--- CompileJS.sh
+++ CompileJS.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
+
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export PATH="$JAVA_HOME/bin:$PATH"
 
-cd ../
+COMMIT_ID=$(git rev-parse --short HEAD)
+COMMIT_DATE=$(git log -1 --format=%cd --date=short)
+
+VERSION_FILE="../desktopRuntime/resources/assets/minecraft/monsoon/version/version.txt"
+
+mkdir -p "$(dirname "$VERSION_FILE")"
+
+echo "Monsoon Client v1.0 Patch 0 ($COMMIT_ID / $COMMIT_DATE)" > "$VERSION_FILE"
+
+echo "Updated version.txt:"
+cat "$VERSION_FILE"
+
+cd ../ || exit 1
+
 chmod +x gradlew
 ./gradlew --no-daemon target_teavm_javascript:assembleMainComponents
